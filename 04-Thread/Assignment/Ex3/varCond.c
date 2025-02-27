8e1cbf4a518c53d2c06419c10a4aa357dc6c4240
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-02-27 16:30:10 +0100

    Create varCond.c, ongoing

---
+++ 04-Thread/Assignment/Ex3/varCond.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+// Shared resource
+int data = 0;
+int data_ready = 0; // Condition flag
+
+// Mutex and condition variable
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+void* producer(void* arg) 
+{
+    srand(time(NULL));
+    
+    for (int i = 0; i < 10; i++)
+    {
+        pthread_mutex_lock(&mutex); // Lock to update shared data
+        data = 1 + rand() % 10; // Generate a random number from 1 to 10
+        data_ready = 1;             // Set condition true
+        pthread_cond_signal(&cond); // Send signal to waiting thread
+        pthread_mutex_unlock(&mutex);
+        sleep(1);
+    }
+    
+    return NULL;
+}
+
+void* consumer(void* arg)
+{
+    pthread_mutex_lock(&mutex); // Lock to check condition
+    while (!data_ready)
+    {
+        pthread_cond_wait(&cond, &mutex); // Wait and unlock mutex
+        printf("Consumer: Got data (%d)\n", data);
+    }
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t prod, cons;
+
+    pthread_create(&prod, NULL, producer, NULL);
+    pthread_create(&cons, NULL, consumer, NULL);
+
+    pthread_join(prod, NULL);
+    pthread_join(cons, NULL);
+
+    return 0;
+}
