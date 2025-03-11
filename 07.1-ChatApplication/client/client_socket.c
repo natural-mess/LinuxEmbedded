9de5161062aeb94cbe4eb7129f65df3fc21085f5
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-03-11 15:43:34 +0100

    Create client_socket.c

---
+++ 07.1-ChatApplication/client/client_socket.c
@@ -0,0 +1,23 @@
+/** @file client_socket.c
+ *  @brief A socket client driver.
+ *
+ *  This file contains function definitions for
+ *  socket client.
+ *
+ *  @author Phuc
+ *  @bug No know bugs.
+ */
+
+/***********
+ * Include *
+ ***********/
+#include "../include/chat.h"
+
+/***********************
+ * Function definition *
+ ***********************/
+void client_socketStart(int portNum)
+{
+    
+}
+
