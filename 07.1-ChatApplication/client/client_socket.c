ca77be53a538c76b1192115391300f48548bbd0a
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-03-14 18:12:08 +0100

    Update client_socket.c

--- 07.1-ChatApplication/client/client_socket.c
+++ 07.1-ChatApplication/client/client_socket.c
@@ -16,8 +16,31 @@
 /***********************
  * Function definition *
  ***********************/
-void client_socketStart(int portNum)
+void client_socketStart(int portNum, char* ipAddr)
 {
-    
+    int server_fd;
+    struct sockaddr_in serv_addr;
+	memset(&serv_addr, '0',sizeof(serv_addr));
+
+    /* Initialize client address */
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port   = htons(portNum);
+    if (inet_pton(AF_INET, ipAddr, &serv_addr.sin_addr) == -1) 
+    {
+        handle_error("client_inet_pton()");
+    }
+	
+    /* Create socket */
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1)
+    {
+        handle_error("client_socket()");
+    }
+	
+    /* Connect to server server*/
+    if (connect(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    {
+        handle_error("client_connect()");
+    }
 }
 
