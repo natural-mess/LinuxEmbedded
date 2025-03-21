4b5af23d95c1a39440a302888fe4bf30842cf8b5
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-03-21 18:06:41 +0100

    Update common.h

--- XX-SensorMonitoringSystem/include/common.h
+++ XX-SensorMonitoringSystem/include/common.h
@@ -1,1 +1,30 @@
+/** @file common.h
+ *  @brief Common definitions (e.g., data structs) 
+ * 
+ *  Contains node data format that are used 
+ *  throughout project
+ *
+ *  @author Phuc
+ *  @bug No known bugs.
+ */
 
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Node format */
+typedef struct 
+{
+    int nodeID, data, time;
+}nodeData;
+
+/* Linked list to store data */
+typedef struct dataList
+{
+    nodeData node;
+    dataList *next;
+}dataList;
+
+#endif /* _COMMON_H */
