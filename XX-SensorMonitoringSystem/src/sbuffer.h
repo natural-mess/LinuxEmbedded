5b3206b317d6a592202a0fc3ca6107bf21455ecc
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-03-21 18:06:12 +0100

    Update sbuffer.h

--- XX-SensorMonitoringSystem/src/sbuffer.h
+++ XX-SensorMonitoringSystem/src/sbuffer.h
@@ -1,1 +1,17 @@
+/** @file sbuffer.h
+ *  @brief Shared mailbox declarations
+ *  
+ *  Declares mailbox functions/variables
+ * 
+ *  @author Phuc
+ *  @bug No known bugs.
+ */
 
+#ifndef _SBUFFER_H
+#define _SBUFFER_H
+
+#include "../include/common.h"
+
+
+
+#endif /* _SBUFFER_H */
