af66bbc0231c586835b75a17889a42ac19204c1e
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-04-01 17:12:33 +0200

    Update connection_manager.c

--- XX-SensorMonitoringSystem/src/connection_manager.c
+++ XX-SensorMonitoringSystem/src/connection_manager.c
@@ -1,6 +1,14 @@
-#include "data_manager.h"
+/** @file connection_manager.c
+ *  @brief Implementation of connection manager
+ *
+ *  Handles connection manager thread
+ *
+ *  @author Phuc
+ *  @bug No known bugs.
+ */
 
-void* data_manager(void* arg)
+#include "connection_manager.h"
+
+void *connection_manager(void *arg)
 {
-    
 }
