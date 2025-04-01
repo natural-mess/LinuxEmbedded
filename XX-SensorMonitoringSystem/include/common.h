7785bcdf8aa9ed3bb400808e685aa0d116e351bc
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-04-01 17:10:29 +0200

    Update common.h

--- XX-SensorMonitoringSystem/include/common.h
+++ XX-SensorMonitoringSystem/include/common.h
@@ -1,7 +1,7 @@
 /** @file common.h
- *  @brief Common definitions (e.g., data structs) 
- * 
- *  Contains node data format that are used 
+ *  @brief Common definitions (e.g., data structs)
+ *
+ *  Contains node data format that are used
  *  throughout project
  *
  *  @author Phuc
@@ -11,9 +11,29 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 
-#define MAX_SENSORS 100
-#define TIMEOUT_SECONDS 3
-#define LOG_FIFO_PATH
+#include "../src/sbuffer.h"
 
+#define MAX_SENSORS 50
+#define TIMEOUT_SECONDS 15
+
+typedef struct
+{
+    sbuffer_t* sb;
+    int port;
+} thread_args_t;
+
+typedef struct
+{
+    int connection_id;
+    char ip[16];
+    int port;
+    time_t last_active;
+    int active;
+} connection_tracking_t;
+
+extern int conn_active_count;
+extern pthread_mutex_t conn_mutex;
+
+volatile sig_atomic_t keep_running = 1;
 
 #endif /* _COMMON_H */
