b141347a0b8c12aadade1248b575986873d08f1f
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-04-01 17:12:56 +0200

    Update log.h

--- XX-SensorMonitoringSystem/src/log.h
+++ XX-SensorMonitoringSystem/src/log.h
@@ -10,12 +10,13 @@
 #ifndef LOG_PROCESS_H
 #define LOG_PROCESS_H
 
-#define LOG_FIFO "logFifo"  // Default FIFO name
+#define LOG_FIFO "logFifo" // Default FIFO name
+#define LOG_FIFO_PATH "logs/gateway.log"
 
 // Runs the log process to read from FIFO and write to log file
 void log_process_run(const char *fifo_path, const char *log_file);
 
 // Sends a log message to the FIFO
-void log_event(const char* msg);
+void log_event(const char *msg);
 
 #endif /* LOG_PROCESS_H */
