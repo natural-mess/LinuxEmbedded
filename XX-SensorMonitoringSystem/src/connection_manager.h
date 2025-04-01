3f66cefa07906396d6acafb9ceeb02f4f5c43c6a
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-04-01 17:12:18 +0200

    Update connection_manager.h

--- XX-SensorMonitoringSystem/src/connection_manager.h
+++ XX-SensorMonitoringSystem/src/connection_manager.h
@@ -1,1 +1,16 @@
+/** @file connection_manager.h
+ *  @brief connection_manager definitions
+ *
+ *  Defines functions for the socket connection thread
+ *
+ *  @author Phuc
+ *  @bug No known bugs.
+ */
+
+#ifndef CONNECTION_MANAGER_H
+#define CONNECTION_MANAGER_H
+
+void* connection_manager(void* arg);
+
+#endif /* CONNECTION_MANAGER_H */
 
