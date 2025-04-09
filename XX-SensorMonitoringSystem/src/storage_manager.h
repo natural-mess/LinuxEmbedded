419dbc8c96fea67d28b919e7b3b5f3ed93c488e0
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-04-09 17:15:01 +0200

    Update storage_manager.h

--- XX-SensorMonitoringSystem/src/storage_manager.h
+++ XX-SensorMonitoringSystem/src/storage_manager.h
@@ -1,6 +1,12 @@
 #ifndef STORAGE_MANAGER_H
 #define STORAGE_MANAGER_H
 
+#include "sbuffer.h"
+#include "log.h"
+#include "threads.h"
+
+#define MAX_RETRIES 3
+
 void* storage_manager(void* arg);
 
 #endif /* STORAGE_MANAGER_H */
